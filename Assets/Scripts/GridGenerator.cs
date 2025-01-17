c582a663f66d4056f43ac7ba4c3863f61b9957d6
Raúl Gutiérrez <gatomail22@proton.me>
2025-01-17 22:29:05 +0100

    Set up the camp generator

--- Assets/Scripts/GridGenerator.cs
+++ Assets/Scripts/GridGenerator.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class GridGenerator : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public int Height;
+	public int Width;
+	public float Margin;
+
+	void Start()
+	{
+		GameObject sqr = Resources.Load<GameObject>("Camp/Square");
+
+		Vector3 sqr_size = sqr.GetComponent<Renderer>().bounds.size;
+		float starting_x = transform.position.x - (((sqr_size.x + Margin) * Width - 1)) / 2;
+		float starting_y = transform.position.y - (((sqr_size.y + Margin) * Height - 1)) / 2;
+
+		for (int i = 0; i < Width; i++) {
+			for (int j = 0; j < Height; j++){
+				float x = starting_x + (i * (sqr_size.x + Margin));
+				float y = starting_y + (j * (sqr_size.y + Margin));
+				Instantiate(sqr, new Vector3(x, y, 0), Quaternion.identity);
+			}
+		}
+
+	}
+
 }
---
+++ Assets/Scripts/Square.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class Square : MonoBehaviour
+{
+	public GameObject Child;
+	public Vector3 Position;
+
+}
